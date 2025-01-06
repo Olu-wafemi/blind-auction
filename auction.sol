2906ee496f66e1f7f6b96f145f2e97fdb4a2ce25
Oluwafemi <femio82@gmail.com>
2025-01-06 18:03:01 +0100

    create-contract

---
+++ auction.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract BlindAuction{
+    struct Bid{
+        bytes32 blindedBid;
+        uint deposit;
+    }
+
+    address payable public beneficiary;
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    mapping(address=> Bid[]) public bids;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    mapping(address=> uint) pendingReturns;
+
+    event AuctionEnded(address winner, uint highestBid);
+
+    error TooEarly(uint time);
+    error TooLate(uint time);
+    error AuctionEndAlreadyCalled();
+
+    
+}
